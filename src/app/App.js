4f36597e6af3cb54dbf38d645349959fc5ace9f3
Golden-dragon <snehosurvey2293@gmail.com>
2018-02-02 13:57:27 +0530

    Initial workspace

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+ 
+export class App extends React.Component {
+    
+    render() {
+        return React.createElement('h1', {},
+                        'Welcome to React');
+        
+    }
+}
